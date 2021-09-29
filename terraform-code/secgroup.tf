fe37a33370687c65205b6a079dd6bbe912d3b509
Tanishq-4 <59949930+Tanishq-4@users.noreply.github.com>
2021-09-29 14:44:49 +0530

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -1,19 +1,50 @@
-resource "aws_security_group" "crypto" {
-  name = "${var.prefix}"
-  tags {
-        Name = "${var.prefix}"
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIATCVA7AIJVKL3UTW2"
+  secret_key = "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"
+}
+
+module "sns_topic" {
+  source  = "terraform-aws-modules/sns/aws"
+  version = "~> 3.0"
+  name  = var.sns_topic_name
+}
+
+resource "aws_sns_topic_subscription" "endpoint1" {
+  topic_arn = module.sns_topic.sns_topic_arn
+  protocol  = var.sns_subsciption_protocol
+  endpoint  = var.sns_subsciption_endpoint_1
+  
+}
+
+resource "aws_sns_topic_subscription" "endpoint2" {
+  topic_arn = module.sns_topic.sns_topic_arn
+  protocol  = var.sns_subsciption_protocol
+  endpoint  = var.sns_subsciption_endpoint_2
+  
+}
+
+module "metric_alarm" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "~> 2.0"
+
+  alarm_name          = var.alarm_name
+  alarm_description   = var.alarm_description
+  comparison_operator = var.comparison_operator
+  datapoints_to_alarm = var.datapoints_to_alarm
+  evaluation_periods  = var.evaluation_periods
+  threshold           = var.threshold
+  period              = var.period
+  unit                = var.unit
+ 
+
+  namespace   = var.namespace
+  metric_name = var.metric_name
+  statistic   = var.statistic
+  dimensions = {
+    FunctionName = var.FunctionName
   }
-  description = "${var.prefix} SG"
-  egress {
-    from_port   = 0
-    to_port     = 65535 # All outbound traffic
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-   }
-   ingress {
-     from_port   = 443
-     to_port     = 443 # All outbound traffic
-     protocol    = "TCP"
-     cidr_blocks = ["0.0.0.0/0"]
-    }
+
+  alarm_actions = [module.sns_topic.sns_topic_arn]
+  
 }
